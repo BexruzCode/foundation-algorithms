5e20ca87202bdcbb1dfaadec359ab45dd823b98b
BexruzCode <fullstack8989@gmail.com>
2026-01-10 19:32:03 +0500

    Day03: add age category checker solution

--- day03/age_category_checker.py
+++ day03/age_category_checker.py
@@ -1,1 +1,14 @@
+def age_category_checker(age):
+    if age < 0:
+        return "Invalid age!"
+    elif age <= 12:
+        return "Child"
+    elif age <= 17:
+        return "Teenager"
+    else:
+        return "Adult"
+
+
+age = int(input("Enter your age: "))
+print(age_category_checker(age))
 
