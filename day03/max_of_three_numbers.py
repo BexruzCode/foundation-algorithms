578f1f21f365dd75a504ec9fa37f5fae34caf927
BexruzCode <fullstack8989@gmail.com>
2026-01-10 19:07:14 +0500

    Add implementation for max of three numbers

--- day03/max_of_three_numbers.py
+++ day03/max_of_three_numbers.py
@@ -1,1 +1,16 @@
+def max_of_three_numbers(a, b, c):
+    if a >= b and a > c:
+        return a
+    elif b > a and b >= c:
+        return b
+    else:
+        return c
+
+
+a = int(input("Enter number A: "))
+b = int(input("Enter number B: "))
+c = int(input("Enter number C: "))
+
+result = max_of_three_numbers(a, b, c)
+print(f"The largest number is -> {result}")
 
