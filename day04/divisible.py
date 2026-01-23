c9cf5399475a54be494267db8188e5ef2b317e6b
BexruzCode <fullstack8989@gmail.com>
2026-01-23 21:38:56 +0500

    Add divisibility check for 5 and 10 (Day 04)

--- day04/divisible.py
+++ day04/divisible.py
@@ -1,1 +1,16 @@
+#! Programming Basics.
+#! Date: 23.01.2026.
+#! Author: Behruz Khaitov.
 
+
+def check_divisibility(number):
+    if number % 5 == 0 and number % 10 == 0:
+        return "Divisible by both 5 and 10."
+    elif number % 5 == 0:
+        return "Divisible by 5 only."
+    else:
+        return "Not divisible by 5 or 10."
+
+
+number = int(input("Enter a number: "))
+print(check_divisibility(number))
