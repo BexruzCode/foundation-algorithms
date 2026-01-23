1815635538f5277555f05c602e78b9f7b655f501
BexruzCode <fullstack8989@gmail.com>
2026-01-23 21:12:31 +0500

    Add grade evaluation program (Day 04)

--- day04/grade.py
+++ day04/grade.py
@@ -1,1 +1,23 @@
+#! Programming Basics.
+#! Date: 23.01.2026.
+#! Author: Behruz Khaitov.
+
+
+def grade(score):
+    if score == 1:
+        return "Very bad"
+    elif score == 2:
+        return "Unsatisfactory"
+    elif score == 3:
+        return "Satisfactory"
+    elif score == 4:
+        return "Good"
+    elif score == 5:
+        return "Excellent"
+    else:
+        return "Invalid grade"
+
+
+score = int(input("Enter the grade (1-5): "))
+print(grade(score))
 
