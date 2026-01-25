f1950231501e4efb2be306656c5c5be6c7394c95
BexruzCode <fullstack8989@gmail.com>
2026-01-25 22:54:30 +0500

    Practice conditional logic with FizzBuzz in Python
    Added a FizzBuzz function to evaluate numbers.

--- day05/fizz_buzz.py
+++ day05/fizz_buzz.py
@@ -1,1 +1,19 @@
+#! Programming Basics.
+#! Date: 25.01.2026.
+#! Author: Behruz Khaitov.
 
+
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+
+
+number = int(input("Enter a number: "))
+result = fizz_buzz(number)
+print(result)
