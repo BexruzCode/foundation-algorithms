8086516e3f19ffd4068160740312ddd8997c0d27
BexruzCode <fullstack8989@gmail.com>
2026-01-26 21:02:34 +0500

    Add function to calculate sum from 1 to N

--- day06/sum_numbers_1_to_n.py
+++ day06/sum_numbers_1_to_n.py
@@ -1,1 +1,21 @@
+#! Programming Basics.
+#! Date: 26.01.2026.
+#! Author: Behruz Khaitov.
+
+
+def sum_numbers(n):
+    total = 0
+    number = 1
+
+    while number <= n:
+        total += number
+        number += 1
+
+    return total
+
+
+n = int(input("Enter N: "))
+result = sum_numbers(n)
+print(result)
+
 
