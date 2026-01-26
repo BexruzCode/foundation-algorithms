b78e763e201e310c2b80b399e749fc65c207cca1
BexruzCode <fullstack8989@gmail.com>
2026-01-26 20:45:42 +0500

    Add while loop to print numbers from 1 to N
    Added a function to print numbers from 1 to N based on user input.

--- day06/print_numbers.py
+++ day06/print_numbers.py
@@ -1,1 +1,20 @@
+# Start number from 1
+number = 1
+
+# Take input from the user
+n = int(input("Enter N: "))
+
+
+# Function to print numbers from 1 to N
+def print_numbers(number):
+    # Loop until number becomes greater than N
+    while number <= n:
+        # Print current number
+        print(number)
+        # Increase number by 1
+        number += 1
+
+
+# Call the function
+print_numbers(number)
 
